0e93343fbd4b734559e1dec0516e7555f443502b
A-DYB <adrian.dybka@mail.mcgill.ca>
2021-03-29 11:35:16 -0400

    Optimization revamp

--- src/orok/ttk/Dot.java
+++ src/orok/ttk/Dot.java
@@ -1,16 +1,42 @@
 package orok.ttk;
 
 public class Dot {
+	/*
+	IDENTIFIERS
+    0 impact 
+    1 puncture
+    2 Slash
+    3 Heat
+    4 Cold
+    5 Electric
+    6 Toxin
+    7 Blast
+    8 Radiation
+    9 Gas
+    10 Magnetic
+    11 Viral
+    12 Corrosive
+    13 
+    14 
+    15 
+    16
+    17
+    18
+    19
+    */
 	private int offset;
 	private int life; //in milliseconds
-	private double damage;
+	private double[] damage;
 	public static double slow = 1;
 	public static int electricity_offset = -1;
+	int identifier;
 	
-	public Dot(int offset, int life, double damage) {
+	
+	public Dot(int offset, int life, double[] damage, int id) {
 		this.offset = offset;
 		this.life = life;
 		this.damage = damage;
+		this.identifier = id;
 	}
 	
 	//life is in ms
@@ -20,7 +46,7 @@ public class Dot {
 	public void setOffset(int offset) {
 		this.offset = offset;
 	}
-	public void setDamage(double damage) {
+	public void setDamage(double[] damage) {
 		this.damage = damage;
 	}
 	public void decLife(int period) {
@@ -32,12 +58,12 @@ public class Dot {
 	public double getLife() {
 		return life;
 	}
-	public double getDamage() {
+	public double[] getDamage() {
 		return damage;
 	}
 	public void reset() {
 		offset =0;
 		life =0;
-		damage =0;
+		damage = new double[15];
 	}
 }
